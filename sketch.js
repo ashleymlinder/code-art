5f82971fd709acb0a537e5706e6a77e4df2f9d98
Ashley Linder <42622331+ashleymlinder@users.noreply.github.com>
2018-11-20 17:17:54 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,40 @@
 var b = 'help'
 var c = "i'm stuck in the void"
+var d = 'with no way out'
+var img= 'stick.png'
 
-function setup() {
-  createCanvas(600, 600);
-  noStroke();
+
+function preload() {
+  img= loadImage("stick.png")
 }
+function setup(){
 
+  createCanvas(1200, 700)
+  noStroke();
+  noCursor();
+}
+ 
 function draw() {
   var x = mouseX;
   var y = mouseY;
   var ix = width - mouseX; // Inverse X
   var iy = height - mouseY; // Inverse Y
+  
+  background(0);
   background(126);
+  image(img,mouseX,mouseY);
+  //bubbles
   fill(255, 150);
-  ellipse(x, height / 2, y, y);
+  ellipse(x, height / 3, y, y);
   fill(0, 159);
   ellipse(ix, height / 2, iy, iy);
+  //text
   fill(240);
   text(b, 15, 15, 50, 50);
   fill(240);
-  text(c, 550, 550, 50, 50);
+  text(c, 590, 350, 50, 50);
+  fill(240); 
+  text(d, 1100, 660, 50, 50);
 }
+
+ 
